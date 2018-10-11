4d373a12b752e5fbb29fe03c482a7b221ea236ec
anavid7 <42363980+anavid7@users.noreply.github.com>
2018-10-10 22:45:44 -0500

    david ryan

---
+++ string_utils.h
@@ -0,0 +1,18 @@
+/**
+* Author(s): David Ryan
+* Date Created: 10/10/2018
+* Date Modified: / /2018
+* Class: CSCE155e
+* Professor: Dr. Chris Bourke
+* NUID: 47810715
+*/
+
+
+
+
+
+void replaceChar(char *s, char oldChar, char newChar);
+char * replaceCharCopy(const char *s, char oldChar, char newChar);
+void removeChar(char *s, char c);
+char * removeCharCopy(const char *s, char c);
+char **lengthSplit(const char *s, int n);
