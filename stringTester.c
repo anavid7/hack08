2211b6b75fc627b81287cbe6e795cf66d0e3b7d1
anavid7 <42363980+anavid7@users.noreply.github.com>
2018-10-11 14:11:24 -0500

    david ryan

--- stringTester.c
+++ stringTester.c
@@ -10,6 +10,18 @@ int main(int argc, char **argv) {
 
 
 
+  int size = 10;
+  char *str = NULL;
+  srt = (char *) malloc(sizeof(char) * size);
+  strcpy(str, "All the world's a stage, and all the men and women merely players: they have their exits and their entrances; and one man in his time plays many parts, his acts being seven ages. ~William Shakespeare");
+
+  void replaceChar(char *s, char oldChar, char newChar)  }
+  char * replaceCharCopy(const char *s, char oldChar, char newChar);
+  void removeChar(char *s, char c);
+  char * removeCharCopy(const char *s, char c);
+  char **lengthSplit(const char *s, int n);
+
+
 
 
 
