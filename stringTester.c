a2e9c85bb95620593a0e6713dfb64dc002daa841
anavid7 <42363980+anavid7@users.noreply.github.com>
2018-10-11 13:59:47 -0500

    david ryan

---
+++ stringTester.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+
+
+
+
+int main(int argc, char **argv) {
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
--- string_utils.c
+++ string_utils.c
@@ -1,23 +1,61 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+#include <strings.h>
 
 
 
 
-
-void replaceChar(char *s, char oldChar, char newChar){
-
+void replaceChar(char *s, char oldChar, char newChar) {
+  int i;
+  int length = strlen(s);
+  for (i=0; i<length; i++) {
+    if (s[i] == oldChar) {
+      s[i] == newChar;
+    }
+  }
 }
+
+
 char * replaceCharCopy(const char *s, char oldChar, char newChar) {
-
+  int length = strlen(s);
+  char *copy = (char *)malloc(sizeof(char) * length+1);
+  char *strcpy = (char *copy, const char *s);
+  replacechar(copy, oldChar, newChar)
+return copy;
 }
 
-void removeChar(char *s, char c){
-
+void removeChar(char *s, char c) {
+  int i;
+  int x = 0;
+  int length = strlen(s);
+  while (i=0; i<length; i++) {
+    if (s[i] != c) {
+      s[x] = s[i];
+      x++;
+    }
+  }
 }
 
-char * removeCharCopy(const char *s, char c){
-
+char * removeCharCopy(const char *s, char c) {
+  char *copy = (char *)malloc(sizeof(char) * length+1);
+  char *strcpy = (char *copy, const char *s);
+  removeChar(copy, c);
+return copy;
 }
 
-char **lengthSplit(const char *s, int n){
-
+char **lengthSplit(const char *s, int n) {
+  int length = strlen(s);
+  int rows = length / n;
+  int **matrix = (int *)malloc(sizeof(char) * rows)
+  int i;
+  int x;
+  int z = 0;
+  for (i=0; i<rows; i++) {
+    for (x=0; x<n; x++) {
+      matrix[i][x] = s[z];
+      z++;
+    }
+  }
+  return matrix;
 }
