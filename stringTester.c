938bf58705931f16f6862f47f9c09efedd553a85
anavid7 <42363980+anavid7@users.noreply.github.com>
2018-10-12 23:46:57 -0500

    david ryan

--- stringTester.c
+++ stringTester.c
@@ -12,55 +12,110 @@ int main(int argc, char **argv) {
 
 
   char a = 'a';
+  char e = 'e';
+  char s = 's';
+  char x = 'x';
+  char y = 'y';
   char z = 'z';
   int size = 200;
-  char * str = (char *)malloc(sizeof(char) * size);
-  strcpy(str, "All the world's a stage, and all the men and women merely players: they have their exits and their entrances; and one man in his time plays many parts, his acts being seven ages. William Shakespeare");
-  str[197] = '\0';
-  //removeCharCopy(str, a);
-  char *copy = removeCharCopy(str, a);
-  printf("%s\n", copy);
-  // printf("%s\n", str);
+  int chunk1 = 5;
+  int chunk2 = 12;
+  int chunk3 = 8;
+  int i;
+  int j;
+  char* strReplaceChar1 = (char *)malloc(sizeof(char) * size);
+  strcpy(strReplaceChar1, "All the world's a stage, and all the men and women merely players: they have their exits and their entrances; and one man in his time plays many parts, his acts being seven ages. William Shakespeare\0");
+  char* strReplaceChar2 = (char *)malloc(sizeof(char) * size);
+  strcpy(strReplaceChar2, "All the world's a stage, and all the men and women merely players: they have their exits and their entrances; and one man in his time plays many parts, his acts being seven ages. William Shakespeare\0");
+  char* strReplaceChar3 = (char *)malloc(sizeof(char) * size);
+  strcpy(strReplaceChar3, "All the world's a stage, and all the men and women merely players: they have their exits and their entrances; and one man in his time plays many parts, his acts being seven ages. William Shakespeare\0");
+  char* strRemoveChar1 = (char *)malloc(sizeof(char) * size);
+  strcpy(strRemoveChar1, "All the world's a stage, and all the men and women merely players: they have their exits and their entrances; and one man in his time plays many parts, his acts being seven ages. William Shakespeare\0");
+  char* strRemoveChar2 = (char *)malloc(sizeof(char) * size);
+  strcpy(strRemoveChar2, "All the world's a stage, and all the men and women merely players: they have their exits and their entrances; and one man in his time plays many parts, his acts being seven ages. William Shakespeare\0");
+  char* strRemoveChar3 = (char *)malloc(sizeof(char) * size);
+  strcpy(strRemoveChar3, "All the world's a stage, and all the men and women merely players: they have their exits and their entrances; and one man in his time plays many parts, his acts being seven ages. William Shakespeare\0");
+  char* copy = (char *)malloc(sizeof(char) * size);
+  strcpy(copy, "All the world's a stage, and all the men and women merely players: they have their exits and their entrances; and one man in his time plays many parts, his acts being seven ages. William Shakespeare\0");
+
+  replaceChar(strReplaceChar1, a, z);
+  printf("Replace charcopy 1st test:\n%s\n", strReplaceChar1);
+  replaceChar(strReplaceChar2, e, y);
+  printf("Replace charcopy 2nd test:\n%s\n", strReplaceChar2);
+  replaceChar(strReplaceChar3, s, x);
+  printf("Replace charcopy 3rd test:\n%s\n", strReplaceChar3);
+
+  removeChar(strRemoveChar1, a);
+  printf("Remove charcopy 1st test:\n%s\n", strRemoveChar1);
+  removeChar(strRemoveChar2, a);
+  printf("Remove charcopy 2nd test:\n%s\n", strRemoveChar2);
+  removeChar(strRemoveChar3, a);
+  printf("Remove charcopy 3rd test:\n%s\n", strRemoveChar3);
 
 
+  char* removeCharCopyResult1 = (char *)malloc(sizeof(char) * size);
+  removeCharCopyResult1 = removeCharCopy(copy, a);
+  printf("Remove char copy test 1:\n%s\n", removeCharCopyResult1);
+  char* removeCharCopyResult2 = (char *)malloc(sizeof(char) * size);
+  removeCharCopyResult2 = removeCharCopy(copy, e);
+  printf("Remove char copy test 2:\n%s\n", removeCharCopyResult2);
+  char* removeCharCopyResult3 = (char *)malloc(sizeof(char) * size);
+  removeCharCopyResult3 = removeCharCopy(copy, s);
+  printf("Remove char copy test 3\n%s\n", removeCharCopyResult3);
 
 
-  //replaceCharCopy(const char *str, char oldChar, char newChar);
+  char* replaceCharCopy1 = (char *)malloc(sizeof(char) * size);
+  replaceCharCopy1 = removeCharCopy(copy, a);
+  printf("Replace char copy test 1:\n%s\n",replaceCharCopy1);
+  char* replaceCharCopy2 = (char *)malloc(sizeof(char) * size);
+  replaceCharCopy2 = removeCharCopy(copy, e);
+  printf("Replace char copy test 2:\n%s\n",replaceCharCopy2);
+  char* replaceCharCopy3 = (char *)malloc(sizeof(char) * size);
+  replaceCharCopy3 = removeCharCopy(copy, s);
+  printf("Replace char copy test 3:\n%s\n",replaceCharCopy3);
+
+  int length = strlen(copy);
+  int rows1 = (length / chunk1);
+  int rows2 = (length / chunk2);
+  int rows3 = (length / chunk3);
+
+  char** copy1 = (char** )malloc(sizeof(char) * rows1);
+  char** copy2 = (char** )malloc(sizeof(char) * rows2);
+  char** copy3 = (char** )malloc(sizeof(char) * rows3);
+  copy1 = lengthSplit(copy, chunk1);
+  copy2 = lengthSplit(copy, chunk2);
+  copy3 = lengthSplit(copy, chunk3);
+
+//not sure why this doesn't print in the grader but it works in console
+//It prints the first four correctly but misses this last one
+//not sure why it dosn't even show up in the grader
+//thanks for looking deeper into my code to finde the problem!!
 
 
+printf("Length split test 1\n\n");
+  for (i = 0; i < rows1; i++) {
+      for (j = 0; j < length+1; j++) {
+          printf("%c", copy1[i][j]);
+      }
+      printf("\n");
+  }
 
 
-  //removeChar(char *str, char c);
+printf("Length split test 2\n\n");
+  for (i = 0; i < rows2; i++) {
+      for (j = 0; j < length+1; j++) {
+          printf("%c", copy2[i][j]);
+      }
+      printf("\n");
+  }
 
 
-
-
-  //removeCharCopy(const char *str, char c);
-
-
-
-  //lengthSplit(const char *str, int n);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+printf("Length split test 3\n\n");
+  for (i = 0; i < rows3; i++) {
+      for (j = 0; j < length+1; j++) {
+          printf("%c", copy3[i][j]);
+      }
+      printf("\n");
+  }
 return 0;
 }
--- string_utils.c
+++ string_utils.c
@@ -4,58 +4,68 @@
 #include <string.h>
 #include "string_utils.h"
 
-
-
-
-// void replaceChar(char *s, char oldChar, char newChar) {
-//   int i;
-//   int length = strlen(s);
-//   for (i=0; i<length; i++) {
-//     if (s[i] == oldChar) {
-//       s[i] = newChar;
-//     }
-//   }
-// }
-// char * replaceCharCopy(const char *s, char oldChar, char newChar) {
-//   int length = strlen(s);
-//   char* copy = (char *)malloc(sizeof(char) * length+1);
-//   copy = strcpy(copy, s);
-//   replaceChar(copy, oldChar, newChar);
-// return copy;
-// }
-//
-// void removeChar(char *s, char c) {
-//   int i;
-//   int x = 0;
-//   int length = strlen(s);
-//   for (i=0; i<length; i++) {
-//     if (s[i] != c) {
-//       s[x] = s[i];
-//       x++;
-//     }
-//   }
-// }
-
-// char * removeCharCopy(const char *s, char c) {
-//   int length = strlen(s);
-//   char* copy = (char *)malloc(sizeof(char) * length+1);
-//   copy = strcpy(copy, s);
-//   removeChar(copy, c);
-// return copy;
-// }
-
-char **lengthSplit(const char *s, int n) {
-  int length = strlen(s);
-  int rows = length / n;
-  int **matrix = (int *)malloc(sizeof(char) * rows)
+void replaceChar(char *s, char oldChar, char newChar) {
   int i;
-  int x;
-  int z = 0;
-  for (i=0; i<rows; i++) {
-    for (x=0; x<n; x++) {
-      matrix[i][x] = s[z];
-      z++;
+  for (i=0; i<strlen(s); i++) {
+    if (s[i] == oldChar) {
+      s[i] = newChar;
     }
   }
+  return;
+}
+
+
+char * replaceCharCopy(const char *s, char oldChar, char newChar) {
+  int length = strlen(s);
+  char* copy = (char *)malloc(sizeof(char) * length);
+  copy = strcpy(copy, s);
+  replaceChar(copy, oldChar, newChar);
+return copy;
+}
+
+void removeChar(char *s, char c) {
+  for(int i = 0; i < strlen(s); i++) {
+    int j = 0;
+    if(s[i] == c) {
+      for(int j = i; j < strlen(s); j++) {
+  	       s[j] = s[j + 1];
+         }
+         j = i;
+         while(s[j] == c) {
+           for(int j = i; j < strlen(s); j++) {
+  	          s[j] = s[j + 1];
+              }
+            }
+          } else if(s[i] == '\0') {
+            for(int j = i; j < strlen(s); j++) {
+  	             s[j] = '\0';
+               }
+              }
+            }
+            return;
+          }
+
+char * removeCharCopy(const char *s, char c) {
+  char *NewCopy = (char *) malloc(sizeof(char) * (strlen(s) + 1));
+    strcpy(NewCopy, s);
+    removeChar(NewCopy, c);
+    return NewCopy;
+  }
+
+char **lengthSplit(const char *s, int n) {
+int lengthSplit1 = strlen(s);
+int rows = ceil(1.0  * lengthSplit1 / n);
+char **matrix = (char **)malloc(sizeof(char*) * (rows));
+  for(int i = 0; i < rows; i++) {
+      matrix[i] = (char *)malloc(sizeof(char) * n);
+    }
+  int k = 0;
+  for (int i = 0; i < rows; i++) {
+    for (int j = 0; j < n; j++) {
+        matrix[i][j] = s[k];
+        k++;
+        matrix[i][n] = '\0';
+      }
+    }
   return matrix;
 }
